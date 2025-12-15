b2c5a36bf5aa40767b97674c31c6bc6c9f814460
Pajser68 <mihaelpokorny@gmail.com>
2025-12-15 19:14:23 +0100

    Render changes.

--- app.py
+++ app.py
@@ -89,4 +89,5 @@ def contact():
 
 if __name__ == '__main__':
     init_db()
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.getenv('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
